150b768ca5a20e0435b59d5e337356c8f1226962
Oleg Skvortsov <highsoft.soi@gmail.com>
2024-07-14 15:55:53 -0700

    Added multiplexor CD4051 for connecting two soil sensors to one A0 port.

--- soil_moisture.ino
+++ soil_moisture.ino
@@ -14,16 +14,23 @@
 #define soil_moisture_topic "homeassistant/sensor/soil_moisture"
 #define soil_moisture_condition_topic "homeassistant/sensor/soil_moisture_condition"
 #define soil_moisture_controller_topic "homeassistant/sensor/soil_moisture_state"
-#define soil_moisture_topic_flower_new "homeassistant/sensor/soil_moisture_flower_new"
-#define soil_moisture_condition_topic_flower_new "homeassistant/sensor/soil_moisture_condition_flower_new"
-#define soil_moisture_controller_topic_flower_new "homeassistant/sensor/soil_moisture_state_flower_new"
+#define soil_moisture_topic1 "homeassistant/sensor/soil_moisture1"
+#define soil_moisture_condition_topic1 "homeassistant/sensor/soil_moisture_condition1"
+#define soil_moisture_controller_topic1 "homeassistant/sensor/soil_moisture_state1"
+//#define soil_moisture_topic_flower_new "homeassistant/sensor/soil_moisture_flower_new"
+//#define soil_moisture_condition_topic_flower_new "homeassistant/sensor/soil_moisture_condition_flower_new"
+//#define soil_moisture_controller_topic_flower_new "homeassistant/sensor/soil_moisture_state_flower_new"
 
-#define sensorPower 2 //D4 on ESP8266 board
-#define sensorPower 5 //D2 on ESP8266 board
+#define sensorPower1 14 //D5 on ESP8266 board
+#define sensorPower 5 //D1 on ESP8266 board
 #define sensorPin A0
 
 #define DHTPIN  4 //D2 on ESP8266 board
 DHT11 dht11(DHTPIN);
+
+#define MUX_A 12 //D6 on ESP8266 board
+#define MUX_B 13 //D7 on ESP8266 board
+#define MUX_C 15 //D8 on ESP8266 board
 
 WiFiClient espClient;
 PubSubClient client(espClient);
@@ -32,9 +39,17 @@ long lastMsg = 0;
 float temp = 0.0;
 float hum = 0.0;
 float diff = 1.0;
+float diff1 = 1.0;
 int soil_moisture = 0;
 String level_moisture = "";
+int soil_moisture1 = 0;
+String level_moisture1 = "";
 
+void changeMux(int c, int b, int a) {
+  digitalWrite(MUX_A, a);
+  digitalWrite(MUX_B, b);
+  digitalWrite(MUX_C, c);
+}
 
 void setup() {
   //Serial.begin(9600);
@@ -43,8 +58,15 @@ void setup() {
   client.setServer(mqtt_server, 1883);
 
   //Soil moisture
-  pinMode(sensorPower, OUTPUT);  
+  pinMode(sensorPower, OUTPUT);
+  pinMode(sensorPower1, OUTPUT); 
   digitalWrite(sensorPower, LOW);
+  digitalWrite(sensorPower1, LOW);
+
+  //Deifne output pins for Mux
+  pinMode(MUX_A, OUTPUT);
+  pinMode(MUX_B, OUTPUT);     
+  pinMode(MUX_C, OUTPUT);  
 }
 
 void setup_wifi() {
@@ -96,12 +118,23 @@ bool checkBoundSoil(float newVal, float prevVal, float maxDiff) {
   return !isnan(newVal) && (newVal < prevVal - maxDiff || newVal > prevVal + maxDiff);
 }
 
-// This function returns the analog soil moisture measurement
+// This function returns the analog soil moisture measurement on Rose flower
 int readSensor() {
   digitalWrite(sensorPower, HIGH);  // Turn the sensor ON
-  delay(10);              // Allow power to settle
-  int val = analogRead(sensorPin);  // Read the analog value form sensor
+  delay(10); // Allow power to settle
+  changeMux(LOW, HIGH, HIGH); //Change multiplexer
+  int val = analogRead(sensorPin); //Value of the sensor connected Option 0 pin of Mux
   digitalWrite(sensorPower, LOW);   // Turn the sensor OFF
+  return val;             // Return analog moisture value
+}
+
+// This function returns the analog soil moisture measurement on New flower
+int readSensor1() {
+  digitalWrite(sensorPower1, HIGH);  // Turn the sensor ON
+  delay(10); // Allow power to settle
+  changeMux(LOW, LOW, HIGH); //Change multiplexer
+  int val = analogRead(sensorPin); //Value of the sensor connected Option 0 pin of Mux
+  digitalWrite(sensorPower1, LOW);   // Turn the sensor OFF
   return val;             // Return analog moisture value
 }
 
@@ -120,6 +153,7 @@ void loop() {
 
     //////////Send state of controller
     client.publish(soil_moisture_controller_topic, "1");
+    client.publish(soil_moisture_controller_topic1, "1");
 
     //////////Temperature and humidity sensor
     float newTemp = (float)dht11.readTemperature();
@@ -142,13 +176,17 @@ void loop() {
     ////////////////////////////////////////////////////////////////
 
 
-    ///////////Soil moisture sensor
+    ///////////Soil moisture sensors
     int new_soil_moisture = readSensor();
     String tempMoisture = level_moisture;
 
+    int new_soil_moisture1 = readSensor1();
+    String tempMoisture1 = level_moisture1;
+
+    //Checking for Rose flower
     if (checkBoundSoil(new_soil_moisture, soil_moisture, diff)) {
       soil_moisture = new_soil_moisture;
-      Serial.print("New soil moisture:");
+      Serial.print("New soil moisture Rose flower:");
       if (soil_moisture < 400) {
         level_moisture = "Soil is extremelly wet!";        
       }
@@ -172,6 +210,36 @@ void loop() {
           
       Serial.println(String(soil_moisture).c_str());
       client.publish(soil_moisture_topic, String(soil_moisture).c_str(), true);
+    }
+
+    
+    //Checking for new flower
+    if (checkBoundSoil(new_soil_moisture1, soil_moisture1, diff1)) {
+      soil_moisture1 = new_soil_moisture1;
+      Serial.print("New soil moisture New flower:");
+      if (soil_moisture1 < 400) {
+        level_moisture1 = "Soil is extremelly wet!";        
+      }
+      if (soil_moisture1 >= 400 && soil_moisture1 <= 600) {
+        level_moisture1 = "Soil is wet!";       
+      }
+      if (soil_moisture1 >= 601 && soil_moisture1 <= 850) {
+        level_moisture1 = "Soil moisture is perfect!";       
+      }
+      if (soil_moisture1 >= 851 && soil_moisture1 <= 970) {
+        level_moisture1 = "Soil is dry!";
+      }
+      if (soil_moisture1 >970) {
+        level_moisture1 = "Soil is extremelly dry!";
+      }
+
+      if (tempMoisture1 != level_moisture1) {
+        Serial.println(level_moisture1);
+        client.publish(soil_moisture_condition_topic1, level_moisture1.c_str());
+      }
+          
+      Serial.println(String(soil_moisture1).c_str());
+      client.publish(soil_moisture_topic1, String(soil_moisture1).c_str(), true);
     }
     
   }
